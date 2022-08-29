4d975007eea307037abefeb96dcfa3762187f4f0
Kevin Souto <kevinsoutods@gmail.com>
2022-08-28 22:34:42 -0300

    Criando e estilizando o Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // components
 import Footer from './components/Footer';
 import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 import styles from './App.module.css';
 
@@ -12,7 +14,14 @@ function App() {
       <Header/>
 
         <main className={styles.main}>
-          <h1>Conteúdo...</h1>
+          <div>
+            <h2>O que você vai fazer?</h2>
+            <TaskForm btnText="Criar Tarefa"/>
+          </div>
+          <div>
+            <h2>Suas Tarefas: </h2>
+            <TaskList/>
+          </div>
         </main>
 
       <Footer/>
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const TaskList = (props: Props) => {
+  return (
+    <div>
+        <h1>TaskList</h1>
+        <p>Lista de Tarefas</p>
+    </div>
+  )
+}
+
+export default TaskList
