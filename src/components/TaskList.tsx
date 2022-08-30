8daabe6547e5c465ccabc4c88298680dbcce1da4
Kevin Souto <kevinsoutods@gmail.com>
2022-08-29 22:54:50 -0300

    Projeto Concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -7,16 +7,25 @@ import { ITask } from '../interface/Task';
 import styles from "./TaskList.module.css";
 
 type Props = {
-    taskList: ITask[]
+    taskList: ITask[];
+    handleDelete(id: number): void;
+    handleEdit(task: ITask): void;
 }
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = ({taskList, handleDelete, handleEdit}: Props) => {
   return (
     <>
         {taskList.length > 0 ? (
             taskList.map((task) => (
-                <div key={task.id}>
-                    <p>{task.title}</p>
+                <div key={task.id} className={styles.task}>
+                    <div className={styles.details}>
+                        <h4>{task.title}</h4>
+                        <p>Dificuldade: {task.difficulty}</p>
+                    </div>
+                    <div className={styles.actions}>
+                        <i className="bi bi-pencil" onClick={() => {handleEdit(task)}}></i>
+                        <i className="bi bi-trash" onClick={() => {handleDelete(task.id)}}></i>
+                    </div>
                 </div>
             ))
         ) : (
